60c8732c8e579e0de96770934ec2f27205e4ea80
Tobias Vidal <tobiasvidald@gmail.com>
2023-07-06 21:28:19 -0300

    Wrap up hitpoint funcionalidad

--- src/components/HitPoints.tsx
+++ src/components/HitPoints.tsx
@@ -8,93 +8,107 @@ function HitPoints({ hitPoints }: { hitPoints: number }) {
         Max,
     };
 
-    const [log, setLog] = useState<{ modif: number, descr: string, type: HitPointType }[]>([]);
+    const [log, setLog] = useState<{ modif: number, descr: string, type: string }[]>([]);
     const [description, setDescription] = useState('');
-    const [modifierType, setModifierType] = useState(HitPointType.Current);
-    const [modifier, setModifier] = useState(0);
+    const [enableHitPointLog, setEnableHitPointLog] = useState(false);
+    const [hitPointType, setHitPointType] = useState(HitPointType.Current);
+    const [hitPointChange, setHitPointChange] = useState('');
     const [temporary, setTemporary] = useState(0);
     const [current, setCurrent] = useState(hitPoints);
     const [max, setMax] = useState(hitPoints);
 
     const hitPointTypeArray = [HitPointType.Temporary, HitPointType.Current, HitPointType.Max];
 
-    const AddModifier = () => {
-        if (!modifier || !description) { return; }
-        setLog([
-            ...log,
-            { modif: modifier, descr: description, type: modifierType }
-        ]);
-        console.log(log);
-        //Aca hacer la logica lel
-
-        let modif = modifier;
+    const AddHitPoints = () => {
+        if (!hitPointChange || !description) { return; }
+        let modif = ConvertToNumber(hitPointChange, 0);
         let newCurrent = current;
         let newTemp = temporary;
         let newMax = max;
+
+        setLog([
+            ...log,
+            { modif, descr: description, type: HitPointType[hitPointType] }
+        ]);
         
         if (modif > 0) {
-            switch (modifierType) {
+            switch (hitPointType) {
                 case HitPointType.Temporary:
                     newTemp += modif;
                     break;
                 case HitPointType.Current:
                     newCurrent += modif;
                     break;
-                    case HitPointType.Max:
+                case HitPointType.Max:
                     newMax += modif;
                     newCurrent += modif;
                     break;
             }
         }
         else {
-            if (modifierType === HitPointType.Max) {
+            if (hitPointType === HitPointType.Max) {
                 newMax += modif;
             }
             newTemp += modif;
-            if (modifierType === HitPointType.Current && newTemp < 0) {
+            if (hitPointType === HitPointType.Current && newTemp < 0) {
                 newCurrent += newTemp;
             }
         }
-        
+
         setTemporary(Math.max(newTemp, 0));
         setCurrent(Math.max(Math.min(newCurrent, newMax), 0));
         setMax(Math.max(newMax, 0));
 
-        setModifier(0);
+        setHitPointChange('');
         setDescription('');
-        setModifierType(HitPointType.Current);
+        setHitPointType(HitPointType.Current);
     }
 
 
     return <div className="mt-4 hitpoints-container px-3">
-        <p className="text-center"><b>Hitpoints</b></p>
-        <div className="row text-center mb-2">
-            <div className="col-2">
-                <input className="form-control text-center px-1" value={modifier} onChange={e => setModifier(ConvertToNumber(e.target.value))} />
-                <label><small>Modifier</small></label>
-            </div>
-            <div className="col-4">
-                <input className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
-                <label><small>Description</small></label>
-            </div>
-            <div className="col-4">
-                <select className="form-control" value={modifierType} onChange={e => setModifierType(ConvertToNumber(e.target.value) as HitPointType)}>
-                    {hitPointTypeArray.map(x =>
-                        <option key={x} value={x}>
-                            {HitPointType[x]}
-                        </option>
-                    )}
-                </select>
-                <label><small>Type</small></label>
-            </div>
-            <div className="col-1 px-0">
-                <button className="btn" onClick={AddModifier}>
-                    <span className="material-icons inline-icon">
-                        add
+        <p className="text-center pt-1">
+            <b className="has-pointer" onClick={() => setEnableHitPointLog(!enableHitPointLog)}>
+                Hitpoints
+                <span className="mx-2">
+                    {!enableHitPointLog && <>+</>}
+                    {enableHitPointLog && <>-</>}
+                </span>
+            </b>
+        </p>
+        {enableHitPointLog &&
+            <div className="d-flex justify-content-around text-center mb-2">
+                <div className="pt-2 has-pointer" onClick={() => console.log(log)}>
+                    <span className="material-icons inline-icons icon-sm" title="log">
+                        article
                     </span>
-                </button>
+                </div>
+                <div className="hp-change-amount">
+                    <input className="form-control text-center px-1" value={hitPointChange} onChange={e => setHitPointChange(e.target.value)} />
+                    <label><small>±Hp</small></label>
+                </div>
+                <div className="hp-change-source">
+                    <input className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
+                    <label><small>Source</small></label>
+                </div>
+                <div className="hp-change-type">
+                    <select className="form-control" value={hitPointType} onChange={e => setHitPointType(ConvertToNumber(e.target.value) as HitPointType)}>
+                        {hitPointTypeArray.map(x =>
+                            <option key={x} value={x}>
+                                {HitPointType[x]}
+                            </option>
+                        )}
+                    </select>
+                    <label><small>Type</small></label>
+                </div>
+                <div>
+                    <button className="btn btn-add-hitpoints px-1" onClick={AddHitPoints}>
+                        <span className="material-icons inline-icon">
+                            add
+                        </span>
+                    </button>
+                </div>
             </div>
-        </div>
+        }
         <div className="d-flex text-center pb-2">
             <div className="ps-4">
                 <input className="form-control text-end" value={temporary} onChange={e => setTemporary(ConvertToNumber(e.target.value))} />
